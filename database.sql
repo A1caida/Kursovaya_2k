d2308f061d338e8c71faf1f5dc29d27ba08102a1
A1caida <46443450+A1caida@users.noreply.github.com>
2021-03-09 19:06:31 +0400

    add triggers and logs

--- database.sql
+++ database.sql
@@ -24,13 +24,14 @@ CREATE TABLE IF NOT EXISTS `author_info` (
   `patronymic` varchar(50) DEFAULT NULL,
   `born` varchar(4) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table biblioteka.author_info: ~1 rows (approximately)
+-- Dumping data for table biblioteka.author_info: ~0 rows (approximately)
 DELETE FROM `author_info`;
 /*!40000 ALTER TABLE `author_info` DISABLE KEYS */;
 INSERT INTO `author_info` (`id`, `surname`, `name`, `patronymic`, `born`) VALUES
-	(1, 'Пушкин', 'Александр', 'Сергеевич', '1799');
+	(1, 'Пушкин', 'Александр', 'Сергеевич', '1799'),
+	(2, 'Есенин', 'Сергей', 'Александрович', '1895');
 /*!40000 ALTER TABLE `author_info` ENABLE KEYS */;
 
 -- Dumping structure for table biblioteka.books
@@ -45,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `books` (
   CONSTRAINT `FK_books_author_info` FOREIGN KEY (`autthor_id`) REFERENCES `author_info` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table biblioteka.books: ~1 rows (approximately)
+-- Dumping data for table biblioteka.books: ~0 rows (approximately)
 DELETE FROM `books`;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
 INSERT INTO `books` (`id`, `autthor_id`, `name`, `year`, `available`) VALUES
@@ -71,6 +72,24 @@ DELETE FROM `borrowed_books`;
 INSERT INTO `borrowed_books` (`id`, `login_id`, `book_id`, `date`) VALUES
 	(1, 1, 1, '2021-03-04');
 /*!40000 ALTER TABLE `borrowed_books` ENABLE KEYS */;
+
+-- Dumping structure for table biblioteka.logss
+CREATE TABLE IF NOT EXISTS `logss` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login_id` int(11) NOT NULL,
+  `oper` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `FK_ban_hammer_user_info` (`login_id`),
+  CONSTRAINT `FK_ban_hammer_user_info` FOREIGN KEY (`login_id`) REFERENCES `user_info` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table biblioteka.logss: ~0 rows (approximately)
+DELETE FROM `logss`;
+/*!40000 ALTER TABLE `logss` DISABLE KEYS */;
+INSERT INTO `logss` (`id`, `login_id`, `oper`) VALUES
+	(1, 14, 'INSERT'),
+	(2, 9, 'BAN');
+/*!40000 ALTER TABLE `logss` ENABLE KEYS */;
 
 -- Dumping structure for table biblioteka.magazine
 CREATE TABLE IF NOT EXISTS `magazine` (
@@ -113,45 +132,31 @@ DELETE FROM `postavshik`;
 /*!40000 ALTER TABLE `postavshik` DISABLE KEYS */;
 /*!40000 ALTER TABLE `postavshik` ENABLE KEYS */;
 
--- Dumping structure for table biblioteka.user
-CREATE TABLE IF NOT EXISTS `user` (
+-- Dumping structure for table biblioteka.user_info
+CREATE TABLE IF NOT EXISTS `user_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `lvl` int(11) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `patronymic` varchar(50) DEFAULT NULL,
+  `phone` varchar(11) NOT NULL,
+  `ban` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `login` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table biblioteka.user: ~3 rows (approximately)
-DELETE FROM `user`;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id`, `login`, `password`, `lvl`) VALUES
-	(1, 'A1caida', 'ch3bur', 3),
-	(2, 'Kveda', '123', 1),
-	(6, 'nya', '321', 1);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-
--- Dumping structure for table biblioteka.user_info
-CREATE TABLE IF NOT EXISTS `user_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login_user` varchar(50) DEFAULT NULL,
-  `surname` varchar(50) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `patronymic` varchar(50) DEFAULT NULL,
-  `phone` varchar(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_user_info_user` (`login_user`),
-  CONSTRAINT `FK_user_info_user` FOREIGN KEY (`login_user`) REFERENCES `user` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
-
--- Dumping data for table biblioteka.user_info: ~3 rows (approximately)
+-- Dumping data for table biblioteka.user_info: ~6 rows (approximately)
 DELETE FROM `user_info`;
 /*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
-INSERT INTO `user_info` (`id`, `login_user`, `surname`, `name`, `patronymic`, `phone`) VALUES
-	(2, 'A1caida', 'Ахмедханов', 'Рамис', 'Нурутдинович', '7934672398'),
-	(3, 'Kveda', 'Микеев', 'Айрат', 'Дамирович', '7935462398'),
-	(4, 'nya', 'Моисеев', 'Александр', 'Витальевич', '79345632365');
+INSERT INTO `user_info` (`id`, `login`, `password`, `lvl`, `surname`, `name`, `patronymic`, `phone`, `ban`) VALUES
+	(5, 'A1caida', 'ch3bur', 3, 'Ахмедханов', 'Рамис', 'Нурутдинович', '79347542389', 0),
+	(6, 'nyaa', 'qsc', 1, 'Моисеев', 'Александр', 'Витальевич', '79347659023', 0),
+	(7, 'Liora', '123', 1, 'Микеев', 'Айрат', 'Дамирович', '79347672356', 0),
+	(8, 'mud', '123', 1, 'Иванов', 'Иван', 'Иванович', '79456236489', 0),
+	(9, 'as', 'asd', 1, 'Фамилия', 'Имя', 'Отчество', '12345678900', 1),
+	(14, 'dsfdsfds', 'sdfsfs', 1, 'sdfdsf', 'sdfdsfds', NULL, '324324', 1);
 /*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
 
 -- Dumping structure for table biblioteka.ychet
@@ -171,27 +176,26 @@ INSERT INTO `ychet` (`id`, `id_oper`, `money`) VALUES
 	(1, 1, '10');
 /*!40000 ALTER TABLE `ychet` ENABLE KEYS */;
 
+-- Dumping structure for trigger biblioteka.ban_hammer
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `ban_hammer` AFTER UPDATE ON `user_info` FOR EACH ROW BEGIN
+if new.ban <> OLD.ban then
+INSERT INTO biblioteka.logss SET logss.login_id = new.id, logss.oper = "BAN";
+END if;
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+-- Dumping structure for trigger biblioteka.logger
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `logger` AFTER INSERT ON `user_info` FOR EACH ROW BEGIN
+INSERT INTO biblioteka.logss SET logss.login_id = NEW.id, logss.oper = "INSERT";
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- --------------------------------------------------------
--- Хост:                         127.0.0.1
--- Версия сервера:               10.4.14-MariaDB - mariadb.org binary distribution
--- Операционная система:         Win64
--- HeidiSQL Версия:              11.2.0.6213
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
--- Экспортируемые данные не выделены.
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
-
